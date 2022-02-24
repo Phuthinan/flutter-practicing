4a6da62d2fabb8e301f57d586c7f9172908ff523
yam <s.phuthinan@gmail.com>
2022-02-24 23:19:14 +0700

    add menu
    create another menu

--- food menu/lib/main.dart
+++ food menu/lib/main.dart
@@ -34,6 +34,11 @@ class _myHomePageState extends State<myHomePage>
     FoodMenu("ก๋วยเตี๋ยวน้ำ", "60", "assets/images/soup.jpg"),
     FoodMenu("ก๋วยเตี๋ยวต้มยำ", "60", "assets/images/tomyum.jpg"),
     FoodMenu("ก๋วยเตี๋ยวเย็นตาโฟ", "60", "assets/images/yenta4.jpg"),
+    FoodMenu("ลูกชิ้นปลา", "10 ลูก 60", "assets/images/fishBall.jpg"),
+    FoodMenu("ลูกชิ้นกุ้ง", "10 ลูก 60", "assets/images/shrimpBall.jpg"),
+    FoodMenu("เผือกทอด", "10 ลูก 50", "assets/images/friedTaro.jpg"),
+    FoodMenu("ฮื่อก้วย", "80", "assets/images/fishStick.jpg"),
+    FoodMenu("ลูกชิ้นปลาทรงเครื่อง", "80", "assets/images/fishBallBig.jpg"),
   ];
 
   @override
@@ -52,7 +57,7 @@ class _myHomePageState extends State<myHomePage>
                   food.name,
                   style: TextStyle(fontSize: 30),
                 ),
-                subtitle: Text("ราคา " + food.price),
+                subtitle: Text("ราคา " + food.price + " บาท"),
                 onTap: () {
                   print("คุณเลือกเมนู " + food.name);
                 },
