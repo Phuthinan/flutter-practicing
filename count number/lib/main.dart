e9d6b383f892ea9680fec4d9449fc3d24ee70ff8
Phuthinan <s.phuthinan@gmail.com>
2022-02-24 18:34:38 +0700

    add menu pic

---
+++ count number/lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+//Stateless เปลี่ยนค่าไม่ได้
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "My App",
+      home: myHomePage(),
+      theme: ThemeData(primarySwatch: Colors.green),
+    );
+  }
+}
+
+class myHomePage extends StatefulWidget {
+  const myHomePage({Key? key}) : super(key: key);
+
+  @override
+  _myHomePageState createState() => _myHomePageState();
+}
+
+class _myHomePageState extends State<myHomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  int number = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("นายฮุยก๋วยเตี๋ยวลูกชิ้นปลา") //Header
+          ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("กดปุ่ม"),
+          Text(
+            number.toString(),
+            style: TextStyle(fontSize: 30),
+          ),
+        ],
+      )), //Body
+      floatingActionButton: FloatingActionButton(
+        onPressed: addnumber,
+        child: Icon(Icons.adb_rounded),
+      ),
+    );
+  }
+
+  void addnumber() {
+    setState(() {
+      number++;
+    });
+  }
+}
---
+++ food menu/lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'foodmenu.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+//Stateless เปลี่ยนค่าไม่ได้
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "My App",
+      home: myHomePage(),
+      theme: ThemeData(primarySwatch: Colors.green),
+    );
+  }
+}
+
+class myHomePage extends StatefulWidget {
+  const myHomePage({Key? key}) : super(key: key);
+
+  @override
+  _myHomePageState createState() => _myHomePageState();
+}
+
+class _myHomePageState extends State<myHomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  List<FoodMenu> menu = [
+    FoodMenu("ก๋วยเตี๋ยวแห้ง", "60", "assets/images/dry.jpg"),
+    FoodMenu("ก๋วยเตี๋ยวน้ำ", "60", "assets/images/soup.jpg"),
+    FoodMenu("ก๋วยเตี๋ยวต้มยำ", "60", "assets/images/tomyum.jpg"),
+    FoodMenu("ก๋วยเตี๋ยวเย็นตาโฟ", "60", "assets/images/yenta4.jpg"),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("เลือกเมนูอาหาร") //Header
+            ),
+        //กำหนด list >10 ค่าขึ้นไป
+        body: ListView.builder(
+            itemCount: menu.length, //จำนวน list
+            itemBuilder: (BuildContext context, int index) {
+              FoodMenu food = menu[index];
+              return ListTile(
+                leading: Image.asset(food.img),
+                title: Text(
+                  food.name,
+                  style: TextStyle(fontSize: 30),
+                ),
+                subtitle: Text("ราคา " + food.price),
+              );
+            }));
+  }
+}
