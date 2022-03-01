5c2f257ac2fa5cad449b8110108a80e65770fd68
yam <s.phuthinan@gmail.com>
2022-03-01 23:21:14 +0700

    Create new project

--- new project/lib/main.dart
+++ new project/lib/main.dart
@@ -1,14 +1,56 @@
-void main() async {
-  print(await loginUser());
-  print("Doing other");
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(MyApp());
 }
 
-Future<String> loginUser() async {
-  var user = await getUserFromDatabase();
-  return "name: " + user;
+//Stateless เปลี่ยนค่าไม่ได้
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "My App",
+      home: myHomePage(),
+      theme: ThemeData(primarySwatch: Colors.lightBlue),
+    );
+  }
 }
 
-//ข้อมูลที่จะได้ต้องรอ 10 วิ
-Future<String> getUserFromDatabase() {
-  return Future.delayed(Duration(seconds: 5), () => "Jinyoung");
+class myHomePage extends StatefulWidget {
+  const myHomePage({Key? key}) : super(key: key);
+
+  @override
+  _myHomePageState createState() => _myHomePageState();
+}
+
+class _myHomePageState extends State<myHomePage> {
+  @override
+  void iniState() {
+    super.initState();
+    getExchangeRate();
+  }
+
+  void getExchangeRate() {
+    print("get exchange rate data");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: Text(
+          "Exchange Rate",
+          style: TextStyle(
+            fontSize: 25,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        )),
+        body: Column(
+          children: [],
+        ));
+  }
 }
