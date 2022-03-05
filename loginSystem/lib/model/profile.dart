0e70ce0978f46d4e4b45f4e1a2cdba29b1d52353
yam <s.phuthinan@gmail.com>
2022-03-05 14:11:12 +0700

    build register page
    - add email keyboard
    - password field change to hide

---
+++ loginSystem/lib/model/profile.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Profile {
+  String email;
+  String password;
+
+  Profile(this.email, this.password);
+}
--- loginSystem/lib/screen/register.dart
+++ loginSystem/lib/screen/register.dart
@@ -8,11 +8,51 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Register"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Container(
+          child: Form(
+            key: formKey,
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Email: ",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  Text(
+                    "Password: ",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  TextField(
+                    obscureText: true,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      child: Text(
+                        "Enter",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      onPressed: () {},
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
