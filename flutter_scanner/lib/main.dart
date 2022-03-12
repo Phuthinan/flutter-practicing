7c681aa8c4e4289a771b53ed102a9e30fb586676
yam <s.phuthinan@gmail.com>
2022-03-13 00:26:09 +0700

    keystore and change main code

--- flutter_scanner/lib/main.dart
+++ flutter_scanner/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qrscan/qrscan.dart' as scanner;
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Scan',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       home: MyHomePage(),
     );
@@ -26,17 +27,56 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String? scanresult;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Barcode & QRCode Scan'),
       ),
-      body: Container(),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Container(
+          child: SizedBox(
+              height: 300,
+              width: double.infinity,
+              child: Card(
+                  child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "ผลการสแกน",
+                      style:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      scanresult ??= "ยังไม่มีข้อมูล",
+                      // [??= "ยังไม่มีข้อมูล"] mean ถ้าไม่มีค่าจะแสดงเป็น ยังไม่มีข้อมูล
+                      style: TextStyle(fontSize: 25),
+                    )
+                  ],
+                ),
+              ))),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: startScan,
         child: const Icon(Icons.qr_code_scanner_sharp),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  startScan() async {
+    //อ่านข้อมูลจาก bar/Qrcode
+    String? cameraScanResult = await scanner.scan();
+    // print(cameraScanResult);
+    setState(() {
+      scanresult = cameraScanResult;
+    });
   }
 }
