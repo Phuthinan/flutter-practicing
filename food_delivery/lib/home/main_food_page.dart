2ff923b004d622e2740aa5d0ce3415ce2838e081
yam <s.phuthinan@gmail.com>
2022-03-14 00:15:18 +0700

    Create new project
    -Write home page
    -create new widget bigtext
    -add font
    -add appcolor

---
+++ food_delivery/lib/home/main_food_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery/widgets/big_text.dart';
+
+import '../utils/colors.dart';
+
+class MainFoodPage extends StatefulWidget {
+  const MainFoodPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainFoodPage> createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            child: Container(
+              margin: EdgeInsets.only(top: 70, bottom: 15),
+              padding: EdgeInsets.only(left: 15, right: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      BigText(
+                        text: "Bangkok",
+                        color: AppColors.mainColor,
+                        size: 50,
+                      ),
+                      Text("City")
+                    ],
+                  ),
+                  Center(
+                    child: Container(
+                      width: 45,
+                      height: 45,
+                      child: Icon(
+                        Icons.search,
+                        color: Colors.white,
+                      ),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: AppColors.mainColor),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
