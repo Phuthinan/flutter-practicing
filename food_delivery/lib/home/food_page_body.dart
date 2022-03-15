fb74d49ea7627d895b2e8e3a6f9d4c3d49876fc4
yam <s.phuthinan@gmail.com>
2022-03-15 22:03:03 +0700

    update slide res

--- food_delivery/lib/home/food_page_body.dart
+++ food_delivery/lib/home/food_page_body.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/utils/colors.dart';
+import 'package:food_delivery/widgets/big_text.dart';
+import 'package:food_delivery/widgets/icon_and_text.dart';
+import 'package:food_delivery/widgets/small_text.dart';
 
 class FoodPageBody extends StatefulWidget {
   const FoodPageBody({Key? key}) : super(key: key);
@@ -8,12 +12,15 @@ class FoodPageBody extends StatefulWidget {
 }
 
 class _FoodPageBodyState extends State<FoodPageBody> {
+  //PageController viewportFraction: ค่ามากยิ่งมองไม่เห็นอันถัดไป
+  PageController pageController = PageController(viewportFraction: 0.85);
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.amber,
+      // color: Colors.amber,
       height: 320,
       child: PageView.builder(
+          controller: pageController,
           itemCount: 5,
           itemBuilder: (context, position) {
             return _buildPageItem(position);
@@ -26,7 +33,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
       children: [
         Container(
           height: 250,
-          margin: EdgeInsets.only(left: 25, right: 25),
+          margin: EdgeInsets.only(left: 5, right: 5),
           decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(30),
               color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
@@ -38,10 +45,65 @@ class _FoodPageBodyState extends State<FoodPageBody> {
             alignment: Alignment.bottomCenter,
             child: Container(
               height: 140,
-              margin: EdgeInsets.only(left: 40, right: 40, bottom: 10),
+              margin: EdgeInsets.only(left: 20, right: 20, bottom: 30),
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(30),
                 color: Colors.white,
+              ),
+              child: Container(
+                padding: EdgeInsets.only(top: 10, left: 15, right: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    BigText(text: "Noodle"),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        Wrap(
+                            children: List.generate(
+                                5,
+                                (index) => Icon(
+                                      Icons.star,
+                                      color: AppColors.mainColor,
+                                      size: 15,
+                                    ))),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        SmallText(text: "4.5"),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        SmallText(text: "1287"),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        SmallText(text: "comments")
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      children: [
+                        IconAndText(
+                            icon: Icons.circle_sharp,
+                            text: "Normal",
+                            iconcolor: AppColors.iconColor1),
+                        IconAndText(
+                            icon: Icons.location_on,
+                            text: "1.7km",
+                            iconcolor: AppColors.mainColor),
+                        IconAndText(
+                            icon: Icons.access_time_rounded,
+                            text: "32min",
+                            iconcolor: AppColors.iconColor2)
+                      ],
+                    )
+                  ],
+                ),
               ),
             ))
       ],
---
+++ food_delivery/lib/widgets/icon_and_text.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery/utils/colors.dart';
+import 'package:food_delivery/widgets/small_text.dart';
+
+class IconAndText extends StatelessWidget {
+  final IconData icon;
+  final String text;
+  final Color iconcolor;
+  const IconAndText(
+      {Key? key,
+      required this.icon,
+      required this.text,
+      required this.iconcolor})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(
+          icon,
+          color: iconcolor,
+        ),
+        SizedBox(
+          width: 5,
+        ),
+        SmallText(
+          text: text,
+        )
+      ],
+    );
+  }
+}
