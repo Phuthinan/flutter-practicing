329644183bd7b02820e496efb760045de8090865
yam <s.phuthinan@gmail.com>
2022-03-22 23:19:29 +0700

    Finish popular detail page

--- food_delivery/lib/pages/food/popular_food_detail.dart
+++ food_delivery/lib/pages/food/popular_food_detail.dart
@@ -4,6 +4,7 @@ import 'package:food_delivery/utils/dimension.dart';
 import 'package:food_delivery/widgets/app_column.dart';
 import 'package:food_delivery/widgets/app_icon.dart';
 import 'package:food_delivery/widgets/big_text.dart';
+import 'package:food_delivery/widgets/expandable_text.dart';
 import 'package:food_delivery/widgets/icon_and_text.dart';
 import 'package:food_delivery/widgets/small_text.dart';
 
@@ -13,8 +14,10 @@ class PopularFoodDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Stack(
         children: [
+          //  background image
           Positioned(
               left: 0,
               right: 0,
@@ -26,6 +29,7 @@ class PopularFoodDetail extends StatelessWidget {
                         fit: BoxFit.cover,
                         image: AssetImage("assets/image/noodle.jpg"))),
               )),
+          //  icon button
           Positioned(
               top: Dimensions.height(45),
               left: Dimensions.width(20),
@@ -37,12 +41,13 @@ class PopularFoodDetail extends StatelessWidget {
                   AppIcon(icon: Icons.shopping_cart_outlined)
                 ],
               )),
+          //  introduction
           Positioned(
               left: 0,
               right: 0,
               top: Dimensions.height(350 - 20),
               child: Container(
-                  height: 200,
+                  height: Dimensions.height(330),
                   padding: EdgeInsets.only(
                       left: Dimensions.width(20),
                       right: Dimensions.width(20),
@@ -61,10 +66,83 @@ class PopularFoodDetail extends StatelessWidget {
                       SizedBox(
                         height: Dimensions.height(20),
                       ),
-                      BigText(text: "Introduce")
+                      BigText(text: "Introduce"),
+                      SizedBox(
+                        height: Dimensions.height(20),
+                      ),
+                      Expanded(
+                        child: SingleChildScrollView(
+                          child: ExpandableText(
+                              text:
+                                  "Noodles are a type of food made from unleavened dough which is rolled flat and cut, stretched or extruded, into long strips or strings. Noodles can be refrigerated for short-term storage or dried and stored for future use. Noodles are usually cooked in boiling water, sometimes with cooking oil or salt added."),
+                        ),
+                      )
                     ],
-                  )))
+                  ))),
         ],
+      ),
+      bottomNavigationBar: Container(
+        height: Dimensions.height(120),
+        padding: EdgeInsets.only(
+            top: Dimensions.height(20),
+            bottom: Dimensions.height(20),
+            left: Dimensions.width(20),
+            right: Dimensions.width(20)),
+        decoration: BoxDecoration(
+            color: AppColors.buttonBackgroundColor,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(Dimensions.height(20)),
+                topRight: Radius.circular(Dimensions.height(20)))),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              padding: EdgeInsets.only(
+                  top: Dimensions.height(20),
+                  bottom: Dimensions.height(20),
+                  right: Dimensions.width(20),
+                  left: Dimensions.width(20)),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(Dimensions.height(20)),
+                  color: Colors.white),
+              child: Row(children: [
+                Icon(
+                  Icons.remove,
+                  color: AppColors.signColor,
+                ),
+                SizedBox(
+                  width: Dimensions.width(5),
+                ),
+                BigText(
+                  text: "0",
+                  size: Dimensions.height(18),
+                ),
+                SizedBox(
+                  width: Dimensions.width(5),
+                ),
+                Icon(
+                  Icons.add,
+                  color: AppColors.signColor,
+                )
+              ]),
+            ),
+            Container(
+              child: BigText(
+                text: "80|Add to cart",
+                size: Dimensions.height(18),
+                color: Colors.white,
+              ),
+              padding: EdgeInsets.only(
+                  top: Dimensions.height(20),
+                  bottom: Dimensions.height(20),
+                  right: Dimensions.width(20),
+                  left: Dimensions.width(20)),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(Dimensions.height(20)),
+                  color: AppColors.mainColor),
+            )
+          ],
+        ),
       ),
     );
   }
---
+++ food_delivery/lib/widgets/expandable_text.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery/utils/colors.dart';
+import 'package:food_delivery/utils/dimension.dart';
+import 'package:food_delivery/widgets/small_text.dart';
+
+class ExpandableText extends StatefulWidget {
+  final String text;
+
+  const ExpandableText({Key? key, required this.text}) : super(key: key);
+
+  @override
+  State<ExpandableText> createState() => _ExpandableTextState();
+}
+
+class _ExpandableTextState extends State<ExpandableText> {
+  late String firstHalf;
+  late String secondHalf;
+
+  bool hiddenText = true;
+
+  double textHeight = Dimensions.height(100);
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.text.length > textHeight) {
+      firstHalf = widget.text.substring(0, textHeight.toInt());
+      secondHalf =
+          widget.text.substring(textHeight.toInt() + 1, widget.text.length);
+    } else {
+      firstHalf = widget.text;
+      secondHalf = "";
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: secondHalf.isEmpty
+          ? SmallText(
+              text: firstHalf,
+              size: Dimensions.height(14),
+              color: AppColors.paraColor,
+            )
+          : Column(
+              children: [
+                SmallText(
+                    text: hiddenText
+                        ? (firstHalf + "...")
+                        : (firstHalf + secondHalf),
+                    size: Dimensions.height(14),
+                    color: AppColors.paraColor),
+                InkWell(
+                  onTap: () {
+                    setState(() {
+                      hiddenText = !hiddenText;
+                    });
+                  },
+                  child: Row(
+                    children: [
+                      SmallText(
+                        text: hiddenText ? "Show more" : "Show less",
+                        color: AppColors.mainColor,
+                        size: Dimensions.height(14),
+                      ),
+                      Icon(
+                        hiddenText
+                            ? Icons.arrow_drop_down
+                            : Icons.arrow_drop_up,
+                        color: AppColors.mainColor,
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+    );
+  }
+}
