a5699eb66b05f996a8b17598fa6153ebecb0911f
yam <s.phuthinan@gmail.com>
2022-03-28 17:19:03 +0700

    finish detail page

--- new_app_1/lib/pages/detail_page.dart
+++ new_app_1/lib/pages/detail_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:new_app_1/utils/colors.dart';
 import 'package:new_app_1/widgets/app_button.dart';
 import 'package:new_app_1/widgets/large_text.dart';
+import 'package:new_app_1/widgets/responsive_button.dart';
 import 'package:new_app_1/widgets/small_text.dart';
 
 class DetailPage extends StatefulWidget {
@@ -27,7 +28,7 @@ class _DetailPageState extends State<DetailPage> {
                 right: 0,
                 child: Container(
                   width: double.maxFinite,
-                  height: 350,
+                  height: 300,
                   decoration: BoxDecoration(
                       image: DecorationImage(
                           image: AssetImage("assets/image/mountain2.jpg"),
@@ -47,7 +48,7 @@ class _DetailPageState extends State<DetailPage> {
               ),
             ),
             Positioned(
-                top: 330,
+                top: 280,
                 child: Container(
                   padding: EdgeInsets.only(left: 20, right: 20, top: 20),
                   width: MediaQuery.of(context).size.width,
@@ -137,9 +138,48 @@ class _DetailPageState extends State<DetailPage> {
                             );
                           },
                         ),
+                      ),
+                      SizedBox(height: 20),
+                      LargeText(
+                        text: "Description",
+                        color: Colors.black.withOpacity(0.8),
+                        size: 20,
+                      ),
+                      Container(
+                        width: double.maxFinite,
+                        height: 100,
+                        child: Expanded(
+                          flex: 1,
+                          child: SingleChildScrollView(
+                            scrollDirection: Axis.vertical,
+                            child: SmallText(
+                                text:
+                                    "อยู่ห่างจากตัวจังหวัดราชบุรีประมาณ 8  กิโลเมตร ตำบลเกาะพลับพลา อำเภอเมือง จังหวัดราชบุรี  แต่เดิมเป็นแหล่งระเบิดและย่อยหินที่สำคัญของไทยตั้งแต่สมัยต้นรัตนโกสินทร์ เนื่องจากเป็นปูนที่มีคุณภาพดี ต่อมาทั้งภาครัฐและประชาชนได้เล็งเห็นถึงความเสื่อมโทรมของสภาพภูมิประเทศ และวิวทิวทัศน์ "),
+                          ),
+                        ),
                       )
                     ],
                   ),
+                )),
+            Positioned(
+                bottom: 20,
+                left: 20,
+                right: 20,
+                child: Row(
+                  children: [
+                    AppButtons(
+                      color: AppColors.textColor,
+                      backgroundColor: Colors.white,
+                      borderColor: AppColors.textColor,
+                      size: 60,
+                      isIcon: true,
+                      icon: Icons.favorite_border,
+                    ),
+                    SizedBox(width: 20),
+                    ResponsiveButton(
+                      isResponsive: true,
+                    )
+                  ],
                 ))
           ],
         ),
--- new_app_1/lib/widgets/responsive_button.dart
+++ new_app_1/lib/widgets/responsive_button.dart
@@ -1,30 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:new_app_1/utils/colors.dart';
+import 'package:new_app_1/widgets/large_text.dart';
+import 'package:new_app_1/widgets/small_text.dart';
 
 class ResponsiveButton extends StatelessWidget {
-  bool isResponsive;
-  double width;
+  bool? isResponsive;
+  double? width;
 
-  ResponsiveButton({Key? key, required this.width, this.isResponsive = false})
+  ResponsiveButton({Key? key, this.width = 120, this.isResponsive = false})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: width,
-      height: 60,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10), color: AppColors.mainColor),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Icon(
-            Icons.skip_next_outlined,
-            color: Colors.black45,
-            size: 50,
-          ),
-        ],
+    return Flexible(
+      child: Container(
+        padding: EdgeInsets.only(left: 10, right: 10),
+        width: isResponsive == true ? double.maxFinite : width,
+        height: 60,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: AppColors.mainColor),
+        child: Row(
+          mainAxisAlignment: isResponsive == true
+              ? MainAxisAlignment.spaceBetween
+              : MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            isResponsive == true
+                ? LargeText(
+                    text: "Book trip now",
+                    size: 20,
+                    color: Colors.black54,
+                  )
+                : Container(),
+            LargeText(
+              text: ">>>>",
+              color: Colors.black54,
+            )
+          ],
+        ),
       ),
     );
   }
